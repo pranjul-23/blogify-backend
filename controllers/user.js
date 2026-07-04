a7e6afb83dd52969b9fad1cb704e66e4be258518
pranjul-23 <pranjul231193@gmail.com>
2026-07-04 13:24:34 +0530

    some cleanup done

--- controllers/user.js
+++ controllers/user.js
@@ -57,7 +57,7 @@ async function handleUserLogin(req, res) {
       .cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 24 * 60 * 60 * 1000,
       })
       .status(200)
--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.CLIENT_URL,
     credentials: true,
   }),
 );
